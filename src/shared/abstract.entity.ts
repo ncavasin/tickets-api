94b6ddc926b57f6fe5464283d970865e74b59137
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-10-22 23:02:50 -0300

    add trackable to abs entity

--- src/shared/abstract.entity.ts
+++ src/shared/abstract.entity.ts
@@ -1,3 +1,38 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 export abstract class AbstractEntity {
-  protected id: BigInteger;
+  get id(): string {
+    return this._id;
+  }
+
+  set id(value: string) {
+    this._id = value;
+  }
+
+  get createdAt(): Date {
+    return this._createdAt;
+  }
+
+  set createdAt(value: Date) {
+    this._createdAt = value;
+  }
+
+  get updatedAt(): Date {
+    return this._updatedAt;
+  }
+
+  set updatedAt(value: Date) {
+    this._updatedAt = value;
+  }
+
+  @PrimaryGeneratedColumn('uuid')
+  protected _id: string;
+  @CreateDateColumn()
+  protected _createdAt: Date;
+  @UpdateDateColumn()
+  protected _updatedAt: Date;
 }
