a187504d3874fc757bf98239ff089a4dbd22ad56
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-10-23 21:02:01 -0300

    add typeorm connection to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,26 @@ import { MovieModule } from './movie/movie.module';
 import { RoomModule } from './room/room.module';
 import { ShowModule } from './show/show.module';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, MovieModule, RoomModule, ShowModule, TagModule],
+  imports: [
+    UserModule,
+    MovieModule,
+    RoomModule,
+    ShowModule,
+    TagModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.EXT_DB_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
