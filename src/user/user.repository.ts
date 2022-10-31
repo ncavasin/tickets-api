0ab014f6fcf61516c9a1ee5419e4976d761f99be
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-10-31 20:11:55 -0300

    add missing constructor

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,14 +1,20 @@
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Builder } from 'builder-pattern';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
+  constructor(private dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+
   async createUser(userCreationDto: CreateUserDto): Promise<User> {
-    const { email, password } = userCreationDto;
-    const createdUser = Builder(User).email(email).password(password).build();
+    const createdUser = Builder(User)
+      .email(userCreationDto.email)
+      .password(userCreationDto.password)
+      .build();
     return await this.save(createdUser);
   }
 }
