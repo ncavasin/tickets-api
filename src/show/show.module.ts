c299bae4e2b7b9e8a91f39776e2a4815fd3adafd
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-10-31 20:11:16 -0300

    remove wrong modules

--- src/show/show.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShowService } from './show.service';
-import { ShowController } from './show.controller';
-
-@Module({
-  controllers: [ShowController],
-  providers: [ShowService],
-})
-export class ShowModule {}
