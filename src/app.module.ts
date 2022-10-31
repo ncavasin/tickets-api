e81dca33b22c2aac4653f2b1c84a0948b23592bb
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-10-31 20:13:03 -0300

    temporally hardcode local creds

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { MovieModule } from './movie/movie.module';
-import { RoomModule } from './room/room.module';
-import { ShowModule } from './show/show.module';
 import { TagModule } from './tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     UserModule,
-    MovieModule,
-    RoomModule,
-    ShowModule,
     TagModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.EXT_DB_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      //host: process.env.POSTGRES_HOST,
+      host: '127.0.0.1',
+      //port: parseInt(process.env.EXT_DB_PORT),
+      port: 5050,
+      //username: process.env.POSTGRES_USER,
+      username: 'user',
+      //password: process.env.POSTGRES_PASSWORD,
+      password: 'ticketing.db',
+      //database: process.env.POSTGRES_DB,
+      database: 'ticketing',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.DB_SYNCHRONIZE === 'true',
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
