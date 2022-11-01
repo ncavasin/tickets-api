81e8a78cf0958ce03b5600ae70aef7ebedcf38c6
Nicolas Cavasin <nicolas.cavasin@cloudhesive.com>
2022-11-01 11:37:46 -0300

    add catch blocks for not found exceptions

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
@@ -17,14 +21,22 @@ export class UserService {
   }
 
   async findById(id: string): Promise<User> {
-    return await this.userRepository.findOne({ where: { id } });
+    return await this.userRepository.findOne({ where: { id } }).catch(() => {
+      throw new NotFoundException('User not found by id.');
+    });
   }
 
   async findByEmail(email: string): Promise<User> {
-    return await this.userRepository.findOne({ where: { email } });
+    return await this.userRepository.findOne({ where: { email } }).catch(() => {
+      throw new NotFoundException('User not found by email.');
+    });
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const found = this.findByEmail(updateUserDto.email);
+    if (found) {
+      throw new BadRequestException('Email already in use!');
+    }
     await this.userRepository.update(id, updateUserDto);
     return await this.findById(id);
   }
